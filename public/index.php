c341f23a8eb6a0ebef0785cb120a245429b55b91
Masato Seki <masatoseki@the-neighbourhood.com>
2017-03-18 18:59:05 +0000

    Test htaccess

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php ?>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="/styles/main.css"/>
   <title>Othello</title>
 </head>
 <body>
-  <h1>Othello Hhh Push push</h1>
+  <h1>Othello Hhh Push</h1>
+  <script src="/scripts/main.js"></script>
 </body>
 </html>
