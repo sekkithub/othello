9598cbdfc38c52dc67de6be3a5325c52090d5b86
Masato Seki <masatoseki@the-neighbourhood.com>
2017-03-19 13:54:57 +0000

    Create othello partial

--- public/index.php
+++ public/index.php
@@ -7,32 +7,8 @@
 </head>
 <body>
   <div class="c-container">
-    <div class="c-container__item">
-      <div class="c-othello  js-othello">
-        <div class="c-othello__item  black  js-othello-item">
-          <img src="/images/black/01.svg" class="c-othello__image  js-othello-image  active" />
-          <img src="/images/black/02.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/03.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/04.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/05.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/06.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/07.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/08.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/black/09.svg" class="c-othello__image  js-othello-image" />
-        </div>
-        <div class="c-othello__item  white  js-othello-item">
-          <img src="/images/white/01.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/02.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/03.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/04.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/05.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/06.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/07.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/08.svg" class="c-othello__image  js-othello-image" />
-          <img src="/images/white/09.svg" class="c-othello__image  js-othello-image" />
-        </div>
-      </div>
-    </div>
+    <?php include ('partial/othello.php'); ?>
+    <?php include ('partial/othello.php'); ?>
   </div>
   <script src="/scripts/main.js"></script>
 </body>
